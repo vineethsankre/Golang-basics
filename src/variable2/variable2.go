e9841a4048550a6db6a43cc4ff2c98e4c1df2f20
vineethsankre <lsvineethsankre@gmail.com>
2023-04-18 23:20:09 +0530

    Uploading all programs

---
+++ src/variable2/variable2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var challenge = "#90DaysofDevOps"
+	const daystotal = 90
+
+	fmt.Println("Welcome to", challenge, "")
+	fmt.Println("This is a", daystotal, "challenge")
+}
